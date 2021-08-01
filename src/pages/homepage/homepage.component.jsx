8e17b7559e55a432aac84086baea3730db30aa6d
AramaDiaz <67924613+AramaDiaz@users.noreply.github.com>
2021-08-01 19:24:29 +0300

    broken our homepage into smaller components, finished styling our menu-item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './homepage.styles.scss';
+import Directory from '../../components/directory-menu/directory.component';
 
 const HomePage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
+
+            <Directory />
+            {/* <div className="directory-menu">
                 <div className="menu-item">
                     <div className="content">
                         <h1 className="title">HATS</h1>
@@ -35,7 +38,7 @@ const HomePage = () => {
                         <span className="subtitle">SHOP NOW</span>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
     }
